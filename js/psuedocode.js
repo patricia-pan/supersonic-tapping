dfae496071c2bd3772deab1f057d19795640c141
Your Name <you@example.com>
2021-01-05 11:28:15 -0800

    Added Tech Stack, Approach Taken, Future updates to Readme. Also published game to githubpages and added URL to Readme.

--- js/psuedocode.js
+++ js/psuedocode.js
@@ -47,3 +47,46 @@
 - Add combo count display
 
 */
+
+## Project Requirements
+https://gawdiseattle.gitbook.io/wdi/11-projects/project-1
+
+## Pitch Guidelines:
+https://www.notion.so/patriciapan/Supersonics-P1-Pitch-Guidelines-5f6c39c888a749be8daaafae83872cae
+
+## Example of solid README.md
+https://github.com/melissay94/gradies-grueling-grind/blob/master/README.md
+
+## Mastering Markdowns
+https://guides.github.com/features/mastering-markdown/
+
+### Websites for designing wireframes
+https://app.diagrams.net/
+figma.com
+
+### Credit for DDR arrow image:
+https://www.deviantart.com/inkjuse/art/DDR-Arrow-111309080
+
+### Credit for editing DDR arrow images:
+https://github.com/ngoldstein51
+
+### Credit for canvas background:
+https://ansimuz.itch.io/cyberpunk-street-environment
+
+### Credit for water gif: 
+https://iblog.dearbornschools.org/vietinghoffpe/2017/05/22/last-week-to-swim-2/
+
+### Credit for pink sparkle gif: 
+https://giphy.com/gifs/reaction-mood-3d63YDxRJRtkZepvGm
+
+### Credit for background music.
+Aerosol Of My Love by Kevin MacLeod
+Link: https://incompetech.filmmusic.io/song/7013-aerosol-of-my-love
+License: https://filmmusic.io/standard-license
+
+Maple Leaf Rag by Kevin MacLeod
+Link: https://incompetech.filmmusic.io/song/5762-maple-leaf-rag
+License: https://filmmusic.io/standard-license
+
+### Credit for fonts:
+https://fonts.google.com/specimen/Bungee+Shade?query=morn&sidebar.open=true&selection.family=Bungee+Shade
